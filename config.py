0fe322da6c9dc92f4b5727ea3a212e77bcb05947
Maalim18 <ali.mohamed@student.moringaschool.com>
2021-06-14 08:09:43 +0300

    add changes

--- config.py
+++ config.py
@@ -3,8 +3,11 @@ import os
 class Config:
 
     
-    UPLOADED_PHOTOS_DEST ='app/static/photos'
+
     
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:069813@localhost/pitches'
     SQLALCHEMY_TRACK_MODIFICATIONS=False
+
+
+
