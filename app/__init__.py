074c42cfdf72abebcc67e3c1aba5f63c7472b937
Maalim18 <ali.mohamed@student.moringaschool.com>
2021-06-14 08:20:11 +0300

    add the changes

---
+++ app/__init__.py
@@ -0,0 +1,8 @@
+from flask_sqlalchemy import SQLAlchemy
+
+def create_app(config_name):
+    app = Flask(__name__)
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+    db.init_app(app)
