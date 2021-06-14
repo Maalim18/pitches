a71d39b094646e941dbffdbca0a394cf47bfa573
Maalim18 <ali.mohamed@student.moringaschool.com>
2021-06-14 19:54:13 +0300

    add changes

---
+++ app/main/forms.py
@@ -0,0 +1,20 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,BooleanField,SubmitField,TextAreaField,SelectField
+from wtforms import ValidationError
+from wtforms.validators import Required,Email,EqualTo
+
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Tell us about you.',validators = [Required()])
+    submit = SubmitField('Submit')
+
+
+class PitchForm(FlaskForm):
+    title = StringField('Pitch Title')
+    category = SelectField(u'Pitch Categories', choices=[('Product', 'Product'), ('Adverts', 'Adverts'),('Interview', 'Interview')])
+    pitch = TextAreaField('Pitch')
+    submit = SubmitField('Submit')    
+
+class CommentForm(FlaskForm):
+    comment = TextAreaField('Comment')
+    submit = SubmitField('Post Comments')
